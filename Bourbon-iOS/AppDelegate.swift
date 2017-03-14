7acb8e92b03698e5a63d59565ee9a8aba7d19fb6
atorres <torres.lyssa@gmail.com>
2017-03-13 17:16:11 -0700

    removing UDP stuff

--- Bourbon-iOS/AppDelegate.swift
+++ Bourbon-iOS/AppDelegate.swift
@@ -18,8 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-    var opieBeaconListener: OPIEBeaconListener?
-    
     var reachability: NetworkReachability?
     
    
@@ -48,7 +46,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-        opieBeaconListener?.stopListening()
         reachability?.stopListening()
     }
 
@@ -64,9 +61,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         // Fire it up when we foreground
-        opieBeaconListener = OPIEBeaconListener.sharedInstance
-        opieBeaconListener?.startListening()
-        
         reachability = NetworkReachability.sharedInstance
         reachability?.startListening()
     }
