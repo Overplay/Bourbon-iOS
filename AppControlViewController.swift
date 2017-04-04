4cdea06d36932348955f473bf28595bded2f06be
Mitch Kahn <mitch@ourglass.io>
2017-04-03 23:03:20 -0700

    Added child view for AppControl. Created WebViewBaseVC for code shared between WebViewVCs.

---
+++ AppControlViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AppControlViewController.swift
+//  Bourbon-iOS
+//
+//  Created by Mitchell Kahn on 4/3/17.
+//  Copyright © 2017 Ourglass. All rights reserved.
+//
+
+import UIKit
+
+class AppControlViewController: WebViewBaseViewController {
+    
+    var displayName: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // it's a jump to a control page
+        if let comps = self.targetUrlString?.components(separatedBy: "displayName=") {
+            
+            if comps.count == 2 {
+                displayName = comps[1]
+            } else {
+                displayName = "???"
+            }
+            
+        }
+        
+        self.title = displayName?.removingPercentEncoding
+    }
+
+    
+
+}
--- DeviceViewController.swift
+++ DeviceViewController.swift
@@ -9,102 +9,41 @@
 import UIKit
 import PKHUD
 
-class DeviceViewController: UIViewController, UIWebViewDelegate {
-    
-    @IBOutlet weak var webView: UIWebView!
+class DeviceViewController: WebViewBaseViewController {
     
     var ogDevice: OGDevice!
+    var appDisplayName: String?
+    var controlAppUrlString: String?
+    
     
     override func viewDidLoad() {
+        targetUrlString = self.ogDevice.getUrl()
         super.viewDidLoad()
         
-        self.automaticallyAdjustsScrollViewInsets = false
-        self.navigationItem.backBarButtonItem = UIBarButtonItem(title:"", style:.plain, target:nil, action:nil)
-
-        self.webView.delegate = self
-        self.webView.scrollView.isScrollEnabled = true
-        self.webView.scrollView.bounces = true
+        self.title = ogDevice.name
         
-        // prevent user from scaling or pinch zooming
-        self.webView.scalesPageToFit = false
-        self.webView.isMultipleTouchEnabled = false
-        
-        // set title on navbar
-        self.navigationItem.title = ogDevice.name
-        
-        // configure request timeout
-        let config = URLSessionConfiguration.default
-        config.timeoutIntervalForResource = TimeInterval(10)
-        
-        self.goToControlPage()
     }
     
-    func goToControlPage() {
-        
-        guard let url = URL(string: self.ogDevice.getUrl()) else {
-            return
-        }
-        
-        let urlReq = URLRequest(url: url,
-                                cachePolicy: .reloadIgnoringLocalAndRemoteCacheData,
-                                timeoutInterval: 10)
-        
-        log.debug("url: \(url)")
-        
-        HUD.show(.labeledProgress(title: "Loading", subtitle: "Please wait"))
-        
-        self.webView.loadRequest(urlReq)
-    }
-    
-    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
-        log.debug("Error loading webview: \(error)")
-        self.webViewDidTimeout()
-    }
     
     func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         
-        let requestUrlString = request.url?.absoluteString
         
-        // check if we need to navigate to a new view controller for the incoming url
-        if (requestUrlString?.range(of: self.ogDevice.getUrl()) != nil) {
-            return true
+        if let rUrl = request.url?.absoluteString,  let _ = rUrl.range(of: "app/control/index.html") {
+            
+            controlAppUrlString = rUrl
+            self.performSegue(withIdentifier: "toAppControl", sender: self)
+            return false
+            
         }
         
-        // self.performSegue(withIdentifier: "toAppControl", sender: requestUrlString!)
-        
-        return false
+        return true;
+    
     }
     
-    func webViewDidTimeout() {
-        HUD.hide()
-        
-        let alertController = UIAlertController(title: "Uh oh!", message: "We were unable to access the device.", preferredStyle: .alert)
-        
-        alertController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: {(alert:UIAlertAction!) in
-            _ = self.navigationController?.popViewController(animated: true)
-        }))
-        
-        alertController.addAction(UIAlertAction(title: "Try again", style: .default, handler: {(alert:UIAlertAction!) in
-            self.goToControlPage()
-        }))
-        
-        self.present(alertController, animated: true, completion: nil)
-    }
-    
-    func webViewDidFinishLoad(_ webView: UIWebView) {
-        HUD.hide()
-    }
-    
-    // MARK: - Navigation
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        HUD.hide()
-        super.viewWillDisappear(animated)
-    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "toAppControl" && sender != nil {
-            // TODO
+            (segue.destination as! WebViewBaseViewController).targetUrlString = controlAppUrlString
         }
     }
 }
